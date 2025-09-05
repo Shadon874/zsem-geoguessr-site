83654f32a0785f33c212d6acb02cb27dc1266a46
TenObcy125 <mszkola125@gmail.com>
2025-09-05 12:32:54 +0200

    2024 memories

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Timeline from './sites/Timeline.jsx'
+import Rok2024 from './sites/2024.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App/>} />
         <Route path='/harmonogram' element={<Timeline/>} />
+        <Route path='/2024' element={<Rok2024/>} />
       </Routes>
      </BrowserRouter>
   </StrictMode>,
