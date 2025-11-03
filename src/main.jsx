f237950e540c4b8547978135d47117365c69c245
shadon874 <erthub99@gmail.com>
2025-11-03 14:13:56 +0100

    responsywnosc wynikow

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import {BrowserRouter, Route, Routes, useLocation} from 'react-router-dom'
 import Timeline from './sites/Timeline.jsx'
 import Rok2024 from './sites/2024.jsx'
 import ReadMore from './sites/ReadMore.jsx'
-import Wyniki from './sites/wyniki.jsx'
 import Leaderboard from './sites/Leaderboard.jsx'
 import Bracket from './components/brackets/Bracket.jsx'
 
@@ -19,7 +18,6 @@ createRoot(document.getElementById('root')).render(
         <Route path='/2024' element={<Rok2024/>} />
         <Route path='/more' element={<ReadMore/>} />
         <Route path="/wyniki" element={<Leaderboard/>} />
-        {/* <Route path="/test" element={<Wyniki/>}></Route> */}
         <Route path='/bracket' element={<Bracket/>}></Route>
       </Routes>
      </BrowserRouter>
